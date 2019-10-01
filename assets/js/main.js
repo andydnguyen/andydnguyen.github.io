a54e1152e13b710eced44e567d535bad2122e756
Andy Nguyen <anguyen1119@gmail.com>
2019-10-01 09:47:45 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-jQuery(document).ready(function($){$('.level-bar-inner').css('width','0');$(window).on('load',function(){$('.level-bar-inner').each(function(){var itemWidth=$(this).data('level');$(this).animate({width:itemWidth},800)})});$('.level-label').tooltip();new GitHubCalendar("#github-graph","andydnguyen");GitHubActivity.feed({username:"andydnguyen",selector:"#ghfeed"})})
+jQuery(document).ready(function($) {
+    /*======= Skillset *=======*/
+    $('.level-bar-inner').css('width', '0');
+    $(window).on('load', function() {
+        $('.level-bar-inner').each(function() {
+            var itemWidth = $(this).data('level');
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+        });
+    });
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    new GitHubCalendar("#github-graph", "andydnguyen");
+    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+    GitHubActivity.feed({ username: "andydnguyen", selector: "#ghfeed" });
+});
