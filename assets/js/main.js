492a7d5fb7584cd93a03dc9b06cb67d094899303
Andy Nguyen <anguyen1119@gmail.com>
2019-10-10 23:11:55 -0400

    Edit skill level percentage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
 jQuery(document).ready(function($) {
-    /*======= Skillset *=======*/
-    $('.level-bar-inner').css('width', '0');
-    $(window).on('load', function() {
-        $('.level-bar-inner').each(function() {
-            var itemWidth = $(this).data('level');
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-        });
+  /*======= Skillset *=======*/
+  $('.level-bar-inner').css('width', '0');
+  $(window).on('load', function() {
+    $('.level-bar-inner').each(function() {
+      var itemWidth = $(this).data('level');
+      $(this).animate(
+        {
+          width: itemWidth
+        },
+        800
+      );
     });
-    /* Bootstrap Tooltip for Skillset */
-    $('.level-label').tooltip();
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "andydnguyen");
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "andydnguyen", selector: "#ghfeed" });
+  });
+  /* Bootstrap Tooltip for Skillset */
+  $('.level-label').tooltip();
+  /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+  new GitHubCalendar('#github-graph', 'andydnguyen');
+  /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+  GitHubActivity.feed({ username: 'andydnguyen', selector: '#ghfeed' });
 });
